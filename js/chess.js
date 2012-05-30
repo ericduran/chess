a78991a39cc0376219420bca36b6ce5b0018322f
Eric J. Duran <eric.duran7@gmail.com>
2012-05-30 01:18:30 -0400

    Using id instead of data attributes and stubbing out random functions

--- js/chess.js
+++ js/chess.js
@@ -1,5 +1,8 @@
 (function () {
-  setUp = function() {
+  /**
+   * Set up the board.
+   */
+  setUpBoard = function() {
     var x, y, col, row, flip;
     col = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     row = [1, 2, 4, 4, 5, 6, 7, 8].reverse();
@@ -8,7 +11,7 @@
       var $row = $('<div>');
       for (y in row) {
         $span = $('<span>').html('&nbsp;');
-        $span.data('loc', col[x] + row[y]);
+        $span.attr('id', col[x] + row[y]);
         $span.addClass((flip ? ' grey' : 'black'));
         $row.append($span);
         flip = !flip;
@@ -17,4 +20,37 @@
       flip = !flip;
     }
   }();
+
+  /**
+   * set up the chess peices in the board.
+   * might just move this to the initial board setup.
+   */
+  setUpPieces = function () {
+  }
+
+  /**
+   * Trying to figure out what a valid move is.
+   * Return true, if allowed, false otherwise.
+   */
+  move = function (peice, currentLocation, requestedLocation) {
+    if (isAvalable(requestedLocation)) {
+      // Figure out if the move is valid for this chess peice.
+    }
+    return false;
+  }
+
+  /**
+   * return wheter a location is avaiable for the current selected piece.
+   */
+  isAvalable = function(location) {
+    // Not sure what I want to use to represent available, data-available=yes:no ?
+    return false;
+  }
+
+  /**
+   * return the selected board location and chess peices currently selected if any.
+   */
+  selectedPiece = function() {
+  }
+
 })();
